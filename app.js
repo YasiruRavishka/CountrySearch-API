477fa135d7a78a46f6759ebc9b795c398a09edf0
Yasiru_Ravishka <YasiruRavishka47@gmail.com>
2024-07-04 00:39:48 +0530

    New version release.

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
-    let tblCountries = document.getElementById("tbl");
+    const tblCountries = document.getElementById("tblBody");
 
-    let tblBody = `<tr>
-                    <th>Name</th>
-                    <th>Flag</th>
-                </tr>`;
-
+    let tblBody = ``;
     data.forEach((element) => {
-      tblBody += `<tr>
+      tblBody += `<tr onclick="searchResult('${element.name.common}')">
                     <td>${element.name.common}</td>
-                    <td><img src="${element.flags.png}" alt=""></td>
-                </tr>`;
+                    <td>${element.capital}</td>
+                    <td><img src="${element.flags.png}" alt="${element.flags.alt}"></td>
+                    <td><a href="${element.maps.googleMaps}"><i class="bi bi-geo-alt-fill text-danger"></i></a></td>
+                  </tr>`;
     });
-
     tblCountries.innerHTML = tblBody;
   });
 
-function serchCountries() {
-  let searchValue = document.getElementById("txtSearchValue").value;
+function searchCountry() {
+  const searchValue = document.getElementById("txtSearchValue").value.toLowerCase();
+  let tableRows = document.querySelectorAll("#tblBody tr");
 
-  let officialName = document.getElementById("officialName");
-  let name = document.getElementById("name");
-  let img = document.getElementById("img");
+  tableRows.forEach((row) => {
+    const countryName = row.querySelector("td").innerHTML.toLowerCase();
+    if (countryName.startsWith(searchValue)) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
+}
 
-  fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
+function searchResult(name) {
+  fetch("https://restcountries.com/v3.1/name/" + name)
     .then((res) => res.json())
     .then((data) => {
-      data.forEach((obj) => {
-        officialName.innerText = obj.name.official;
-        name.innerText = obj.name.common;
+      console.log(data);
+      const searchResults = document.getElementById("searchResults");
+      const officialName = document.getElementById("officialName");
+      const capital = document.getElementById("capital");
+      const img = document.getElementById("img");
 
-        img.innerHTML = `<img src="${obj.flags.png}" alt="">`;
-      });
+      searchResults.style.backgroundColor = "whitesmoke";
+      officialName.innerHTML = data[0].name.official;
+      capital.innerHTML = data[0].capital;
+      img.innerHTML = `<img class="img-fluid" src="${data[0].flags.png}" alt="${data[0].flags.alt}">`;
     });
 }
